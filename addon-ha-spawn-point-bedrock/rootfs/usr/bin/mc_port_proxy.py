5a2764792c4085b63be2b9041df5785b2b14b56b
aph-le <alexanderphilipp@gmx.net>
2023-11-12 20:44:52 +0000

    Added example python script Started download server file

---
+++ addon-ha-spawn-point-bedrock/rootfs/usr/bin/mc_port_proxy.py
@@ -0,0 +1,51 @@
+import socket
+import sys
+
+sys.stdout.write('Stdout\n') ; sys.stdout.flush()
+#sys.stderr.write('Stderr \n') ; sys.stderr.flush()
+
+HOST = '0.0.0.0'                 # Symbolic name meaning all available interfaces
+PORT = 19132             # Arbitrary non-privileged port
+
+SERVER_IP = "192.168.178.201"
+SERVER_PORT = 19132
+
+server_address = (SERVER_IP, SERVER_PORT)
+client_address = ("",0)
+
+sys.stdout.write('Waiting for Client -StdOut\n') ; sys.stdout.flush()
+print("Waiting for Client")
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as s:
+    s.bind((HOST, PORT))
+    s.setblocking(True)
+    while True:
+        sys.stdout.write('Enter Loop\n') ; sys.stdout.flush()
+        bytes_, adress_ = s.recvfrom(1024 * 1024 * 8)
+
+        tartByte = bytes_[0]
+        if bytes_[0] == 1:
+            sys.stdout.write(f"Found Client ip: {adress_[0]} on port: {adress_[1]}"); sys.stdout.flush()
+            client_address = adress_
+
+            s.sendto(bytes_, server_address)
+            s.settimeout(5)
+            bytes_, adress_ = s.recvfrom(1024 * 1024 * 8)
+            s.settimeout(None)
+            if bytes_[0] == 0x1c and adress_ == server_address:
+                sys.stdout.write("Got ping response from server"); sys.stdout.flush()
+                s.sendto(bytes_, client_address)
+        elif bytes_[0] == 5:
+            client_address = adress_
+            sys.stdout.write(f"Got connection {adress_[0]} on port: {adress_[1]}"); sys.stdout.flush()
+            break
+
+    s.settimeout(None)
+    while True:
+        bytes_, adress_ = s.recvfrom(1024 * 1024 * 8)
+        print(f"Rec Size {len(bytes_)}")
+        if adress_ == server_address:
+            size = s.sendto(bytes_, client_address)
+        elif adress_ == client_address:
+            size = s.sendto(bytes_, server_address)
+        else:
+            pass
