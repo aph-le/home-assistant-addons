8361cd9f87a27e574e87f17f7202b6938a14f5f6
aph-le <alexanderphilipp@gmx.net>
2023-11-20 14:41:15 +0000

    Started again

--- addon-ha-spawn-point-bedrock/rootfs/etc/services.d/example/hasp_user.sh
+++ addon-ha-spawn-point-bedrock/rootfs/etc/services.d/example/hasp_user.sh
@@ -2,16 +2,12 @@
 #!/usr/bin/with-contenv bashio
 
 function hasp::user() {
-    local ha_config_file=${1}
-    local mc_config_file=${2}
-
-    if [[ ! -f "${ha_config_file}" ]]; then
-        bashio::log.debug  "ha_config_file NOT FOUND!"
-        return "${__BASHIO_EXIT_NOK}"
-    fi
+    local mc_config_file=${1}
 
     if [[ ! -f "${mc_config_file}" ]]; then
         bashio::log.debug  "ha_config_file NOT FOUND!"
         return "${__BASHIO_EXIT_NOK}"
     fi
+
+
 }
