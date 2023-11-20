fad6b839fb5be54777f4a986d1ee53175b4ac369
aph-le <alexanderphilipp@gmx.net>
2023-11-20 20:08:15 +0100

    Can iterate uallow users.

--- addon-ha-spawn-point-bedrock/rootfs/etc/services.d/example/hasp_user.sh
+++ addon-ha-spawn-point-bedrock/rootfs/etc/services.d/example/hasp_user.sh
@@ -8,4 +8,21 @@ function hasp::user() {
         bashio::log.debug  "ha_config_file NOT FOUND!"
         return "${__BASHIO_EXIT_NOK}"
     fi
+
+    # Set username and password for the broker
+    bashio::log "Creating allow List"
+    for mc_user in $(bashio::config 'allow_user|keys'); do
+        username=$(bashio::config "allow_user[${mc_user}].username")
+        xuid=$(bashio::config "allow_user[${mc_user}].xuid")
+
+        bashio::log "Setting up user ${username} - ${xuid}"
+        #if ! bashio::config.true "logins[${login}].password_pre_hashed"
+        #then
+        #    password=$(pw -p "${password}")
+        #else
+        #    bashio::log.info "Using pre-hashed password for ${username}"
+        #fi
+        #echo "${username}:${password}" >> "${PW}"
+        #echo "user ${username}" >> "${ACL}"
+    done
 }
