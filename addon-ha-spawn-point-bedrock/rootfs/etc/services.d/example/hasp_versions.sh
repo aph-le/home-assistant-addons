b3160aa786d3a7a28af12ffb4d2ab32833adb03b
Alexander Philipp <aph-le@users.noreply.github.com>
2023-11-27 22:21:58 +0000

    Got download link from json database.

--- addon-ha-spawn-point-bedrock/rootfs/etc/services.d/example/hasp_versions.sh
+++ addon-ha-spawn-point-bedrock/rootfs/etc/services.d/example/hasp_versions.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
+#!/usr/bin/with-contenv bashio
 
 function hasp::version() {
     local input_version=${1}
+    local download_link=""
 
     if [ "$input_version" = "LATEST" ]; then
         input_version="LATEST"
+    else
+        download_link=$(cat /usr/bin/download_versions.json | jq -r -c '.[] | select(.bedrock_version | contains("1.9.0")) | .download ')
     fi
-
+    echo "${download_link}"
 }
