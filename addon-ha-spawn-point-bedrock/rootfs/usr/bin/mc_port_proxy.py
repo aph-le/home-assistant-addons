a975f79de76f973924db348ab78828c08497e7f9
Alexander Philipp <aph-le@users.noreply.github.com>
2023-12-05 16:30:02 +0000

    Removed single port exposture

--- addon-ha-spawn-point-bedrock/rootfs/usr/bin/mc_port_proxy.py
+++ addon-ha-spawn-point-bedrock/rootfs/usr/bin/mc_port_proxy.py
@@ -7,24 +7,24 @@ sys.stdout.write('Stdout\n') ; sys.stdout.flush()
 HOST = '0.0.0.0'                 # Symbolic name meaning all available interfaces
 PORT = 19132             # Arbitrary non-privileged port
 
-SERVER_IP = "192.168.178.201"
-SERVER_PORT = 19132
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 19135
 
 server_address = (SERVER_IP, SERVER_PORT)
 client_address = ("",0)
 
 sys.stdout.write('Waiting for Client -StdOut\n') ; sys.stdout.flush()
-print("Waiting for Client")
+#print("Waiting for Client")
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as s:
     s.bind((HOST, PORT))
     s.setblocking(True)
     while True:
-        sys.stdout.write('Enter Loop\n') ; sys.stdout.flush()
+        #sys.stdout.write('Enter Loop\n') ; sys.stdout.flush()
         bytes_, adress_ = s.recvfrom(1024 * 1024 * 8)
 
         tartByte = bytes_[0]
         if bytes_[0] == 1:
-            sys.stdout.write(f"Found Client ip: {adress_[0]} on port: {adress_[1]}"); sys.stdout.flush()
+            sys.stdout.write(f"Found Client ip: {adress_[0]} on port: {adress_[1]}\n"); sys.stdout.flush()
             client_address = adress_
 
             s.sendto(bytes_, server_address)
@@ -32,11 +32,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as s:
             bytes_, adress_ = s.recvfrom(1024 * 1024 * 8)
             s.settimeout(None)
             if bytes_[0] == 0x1c and adress_ == server_address:
-                sys.stdout.write("Got ping response from server"); sys.stdout.flush()
+                sys.stdout.write("Got ping response from server\n"); sys.stdout.flush()
                 s.sendto(bytes_, client_address)
         elif bytes_[0] == 5:
             client_address = adress_
-            sys.stdout.write(f"Got connection {adress_[0]} on port: {adress_[1]}"); sys.stdout.flush()
+            sys.stdout.write(f"Got connection {adress_[0]} on port: {adress_[1]}\n"); sys.stdout.flush()
             break
 
     s.settimeout(None)
